13190e360926a3f9650134b15e86c8ba3f70bebd
Satya Santika <55965465+satyasantika@users.noreply.github.com>
2025-02-18 11:55:19 +0700

    spatie role permission implemented

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +22,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'nidn',
+        'nip',
+        'phone',
     ];
 
     /**
